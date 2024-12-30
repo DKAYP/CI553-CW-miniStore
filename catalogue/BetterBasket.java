68ebaec96cdb3aa9a5643ff561fa4b004c33ca70
Daniel <Daniel@10.247.1.39>
2024-12-30 13:14:52 +0000

    Initial changes to class BetterBasket in new branch

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -13,7 +13,7 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
   
-
+  // Example Change
   // You need to add code here
   // merge the items for same product,
   // or sort the item based on the product number
