ff41e688ab53c157b30a420779770ea6fb1069c7
danie <danie@LAPTOP-AB6J7M0K>
2025-01-15 23:30:17 +0000

    lecture 4 tasks

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -17,4 +17,39 @@ public class BetterBasket extends Basket implements Serializable
   // You need to add code here
   // merge the items for same product,
   // or sort the item based on the product number
+  
+  @Override
+  public boolean add( Product newProduct )
+  {               
+	  int i = 0;
+	  int countProducts = this.size();
+	  
+	  // loop to look at every location in the arraylist
+	  for(i = 0; i < countProducts; i++)
+	  {
+		  //get the product from the basket
+		  Product basketProduct = this.get(i);
+		  
+		  // check if the product numbers match
+		  if (basketProduct.getProductNum() == newProduct.getProductNum())
+		  {
+			  // increase the quantity
+			  basketProduct.setQuantity(1 + basketProduct.getQuantity());
+			  // save it back in the array
+			  this.set(i, basketProduct);
+			  // no point checking anything else so return
+			  return true;
+		  }
+	  }
+	// we didnt find the product in the basket so add it instead
+    return super.add( newProduct );   
+  }
+  
+  @Override
+  public String getDetails()
+  {
+	  //added  a comparator function to Product class so that java can sort the array
+	  this.sort(null);
+	  return super.getDetails();
+  }
 }
